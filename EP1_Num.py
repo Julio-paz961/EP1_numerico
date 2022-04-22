e80eafb387109efc26860bf09f33bb2abbc1b4d6
Julio <julio.paz961@usp.br>
2022-04-22 20:25:15 -0300

    resultado final

--- EP1_Num.py
+++ EP1_Num.py
@@ -1,5 +1,29 @@
 import math 
 import numpy as np
+import math
+
+def gera_matriz_cic(n):
+    a=[]
+    b=[]
+    c=[]
+    d=[]
+    M=[]
+    for i in range(1,n+1,1):
+        if i!=n+1:
+            a.append((2*i-1)/4/i)
+            c.append(1-a[i-1])
+            b.append(2)
+            d.append(math.cos((2*math.pi*(i**2))/((n)**2)))
+        else:
+            a.append(((2*(n)-1)/2/(n)))
+            c.append(1-a[i-1])
+            b.append(2)
+            d.append(math.cos((2*math.pi*(i**2))/((n)**2)))
+    M.append(a)
+    M.append(b)
+    M.append(c)
+    return M,d
+
 
 def decomposicao_LU(A):
     A=np.array(A)
@@ -50,38 +74,60 @@ def decomposicao_LU_tri(n,A,d):
     a=A[0]
     b=A[1]
     c=A[2]
+    d=d
+
     l=np.zeros(n)
-    u=np.zeros(n)
     y=np.zeros(n)
     x=np.zeros(n)
-    u[0]=c[0]/b[0]
-    y[0]=d[0]/b[0]
+    
+    l[0]=c[0]/b[0]
+    for i in np.arange(1,n-1,1):  
+       l[i]=c[i]/(b[i]-a[i]*l[i-1])  
+ 
+    y[0]=d[0]/b[0]  
+    for i in np.arange(1,n,1):  
+       y[i]=(d[i]-a[i]*y[i-1])/(b[i]-a[i]*l[i-1])  
+ 
+    #faz substituicao reversa para obter a solucao x  
+    x[n-1]=y[n-1]  
+    for i in np.arange(n-2,-1,-1):  
+       x[i]=y[i]-l[i]*x[i+1]  
 
-    for i in range (1,n,1):
-        #print("i:"+str(i))
-        l[i]=a[i]/u[i-1]
-        #print("li"+str(i)+":"+str(li))
-        u[i]=b[i]-l[i]*c[i-1]
-        #print("ui"+str(i)+":"+str(ui))
-        y[i]=d[i]-l[i]*c[i-1]
-    x[n-1]=(y[n-1])/(u[n-1])
-    for i in range(n-2,-1,-1):
-        x[i]=(y[i]-c[i]*x[i+1])/u[i]
+    return l,y,x
 
 
-    return l,u,y,x
+def decomposicao_LU_tri_cic(n,A,d):
+    a=A[0]
+    b=A[1]
+    c=A[2]
+    Tn=[np.copy(a[:(len(a)-1)]),np.copy(b[:(len(b)-1)]),np.copy(c[:(len(c)-1)])]
+    Sol_Tn=np.copy(d[:(len(d)-1)])
+    v=np.zeros(n-1,dtype=float)
+    w=np.zeros(n-1,dtype=float)
+    v[0]=a[0]
+    v[n-2]=c[n-2]
+    w[0]=c[n-1]
+    w[n-2]=a[n-1]
+    
+    y_barra=decomposicao_LU_tri(n-1,Tn,Sol_Tn)[2]
+    z_barra=decomposicao_LU_tri(n-1,Tn,v)[2]
+    xn=(d[n-1]-c[n-1]*y_barra[0]-a[n-1]*y_barra[n-2])/(b[n-1]-c[n-1]*z_barra[0]-a[n-1]*z_barra[n-2])
+    x_barra=y_barra-xn*z_barra
+    
+    return np.append(x_barra,xn)
+
+    
+#matriz=[[5,4,0,0,0,0],[1,3,1,0,0,0],[0,2,4,1,0,0],[0,0,1,2,1,0],[0,0,0,2,3,2],[0,0,0,0,1,2]]
+#solução=[13,10,20,16,35,17]
+#matriz=[[2,1,0,0,0],[1,2,1,0,0],[0,1,2,1,0],[0,0,1,2,1],[0,0,0,1,2]]
+#solução=[4,4,0,0,2]
+matriz=[[2,1,0,0,0],[1,2,1,0,0],[0,1,2,1,0],[0,0,1,2,1],[0,0,0,1,2]]
+solução=[4,4,0,0,2]
 
 
-matriz=[[5,4,0,0,0,0],[1,3,1,0,0,0],[0,2,4,1,0,0],[0,0,1,2,1,0],[0,0,0,2,3,2],[0,0,0,0,1,2]]
-solução=[13,10,20,16,35,17]
-print("A: ","\n", str(np.array(matriz)))
-D=matriz_tri(matriz)
-print("a: "+str(D[0]))
-print("b: "+str(D[1]))
-print("c: "+str(D[2]))
-print("d: "+str(solução))
-L,U,y,x=decomposicao_LU_tri(len(matriz),D,solução)
-print("L: "+str(L))
-print("U: "+str(U))
-print("y: "+str(y))
+D=gera_matriz_cic(5)
+print(D[0])
+print(D[1])
+x=decomposicao_LU_tri_cic(len(D[0][0]),D[0],D[1])
 print("x: "+str(x))
+
